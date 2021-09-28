de001d174d62708b3e025d19243a90d59657fa76
qbarefoot <quintenbarefoot@gmail.com>
2021-09-28 11:40:07 -0500

    added pbf1.5_basic_lists

---
+++ pbf1.5_basic_lists.py
@@ -0,0 +1,21 @@
+#making a Python list is simple all examples in it can be related or unrelated, run it!
+garden = ["beans", "peppers", "tomatoes", "onions", "basil"]
+print(garden)
+
+#you can access a certain elements in a list by listing the index in the print statement, run it!
+garden = ["beans", "peppers", "tomatoes", "onions", "basil"]
+print(garden[0])
+print(garden[3])
+
+#in Python you can access the last element in a last by using -1, run it!
+garden = ["beans", "peppers", "tomatoes", "onions", "basil"]
+print(garden[-1])
+
+#you can append a element in the list, that we learned previously, run it!
+garden = ["beans", "peppers", "tomatoes", "onions", "basil"]
+print(garden[3].upper())
+
+#you can pull an element from a list to insert in to a "f-string", run it!
+garden = ["beans", "peppers", "tomatoes", "onions", "basil"]
+information = (f"a vegetable that is high yield is, {garden[2].upper()}!")
+print(information)
